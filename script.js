a0144cfb4bb10ae76d2ff2a564c033eda890988b
Ani1210 <a.mamukashvili2016@gmail.com>
2023-02-24 08:56:33 -0500

    lesson13 exercise uploaded

---
+++ script.js
@@ -0,0 +1,43 @@
+console.log("hello from js file");
+
+let myText= "Text";
+console.log(typeof(myText));
+
+const myNumber= 10;
+console.log(typeof (myNumber));
+
+const myBool1= true;
+console.log(typeof (myBool1));
+
+const myBool2= false;
+console.log(typeof (myBool2));
+
+let  myUndefined = undefined;
+console.log(typeof (undefined));
+
+let myNull=null;
+console.log(typeof (null));
+
+const myName = "ana";
+let myHobby = "watching animes";
+const yearOfBirth = "1996";
+let currentYear = "2023";
+
+const result1= "My name is " + myName;
+console.log(result1);
+
+const result2= `My name is ${myName}`;
+console.log(result2)
+
+const result3="I am " + (currentYear - yearOfBirth) + " old ";
+console.log(result3);
+
+const result4=`I am ${currentYear-yearOfBirth} old`;
+console.log(result4);
+
+const result5= "my hobby is " + myHobby;
+console.log(result5);
+
+const result6= `my hobby is ${myHobby}`;
+console.log(result6);
+
